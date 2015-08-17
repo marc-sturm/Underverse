e0ce1d4f3dcb2da4355ed37a12a29e3d594bf916
Marc <Sturm>
2015-08-17 22:01:33 +0200

    Added simple file handling.

--- src/Underverse/mainwindow.cpp
+++ src/Underverse/mainwindow.cpp
@@ -1,8 +1,12 @@
 #include "ui_mainwindow.h"
 
 #include <QMessageBox>
+#include <QFileDialog>
 
 #include "MainWindow.h"
+#include "Settings.h"
+#include "Helper.h"
+#include "Exceptions.h"
 
 #include <markdown.h>
 #include <html.h>
@@ -15,7 +19,14 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    connect(ui->plain, SIGNAL(textChanged()), this, SLOT(updateHtml()));
+    connect(ui->plain, SIGNAL(textChanged()), this, SLOT(textChanged()));
+
+    if (qApp->arguments().count()==2)
+    {
+        loadFile(qApp->arguments().at(1));
+    }
+
+    updateRecentFilesMenu();
 }
 
 MainWindow::~MainWindow()
@@ -23,18 +34,104 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_actionAbout_triggered(bool)
+void MainWindow::on_actionAbout_triggered()
 {
     QMessageBox::about(this, "About " + QApplication::applicationName(), QApplication::applicationName() + " " + QApplication::applicationVersion() +"\n\nThis program is free software.\n\nThis program is provided as is with no warranty of any kind, including the warranty of design, merchantability and fitness for a particular purpose.");
 }
 
-void MainWindow::updateHtml()
+void MainWindow::on_actionOpen_triggered()
 {
+    QString filename = QFileDialog::getOpenFileName(this, "Open file", Settings::string("open_folder", QCoreApplication::applicationDirPath()),"MarkDown files (*.md);;All files (*.*)");
+    if (filename=="") return;
+
+    Settings::setString("open_folder", QFileInfo(filename).canonicalPath());
+    loadFile(filename);
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    if (!modified_) return;
+
+    Helper::storeTextFile(file_, QStringList() << ui->plain->toPlainText());
+    modified_ = false;
+    updateWindowTitle();
+}
+
+void MainWindow::on_actionClose_triggered()
+{
+    on_actionSave_triggered();
+    loadFile("");
+}
+
+void MainWindow::textChanged()
+{
+    //update html
     QString text = ui->plain->toPlainText();
     text = markdown(text);
     text.prepend("<html><head><meta charset=\"utf-8\"><link type=\"text/css\" rel=\"stylesheet\" href=\"qrc:/Resources/github.css\"/></head><body>");
     text.append("</body></html>");
     ui->html->setHtml(text);
+
+    //update window title
+    modified_ = true;
+    updateWindowTitle();
+}
+
+void MainWindow::openRecentFile()
+{
+    QAction* action = qobject_cast<QAction*>(sender());
+    loadFile(action->text());
+}
+
+void MainWindow::loadFile(QString filename)
+{
+    file_ = filename;
+    if (filename=="")
+    {
+        ui->plain->setPlainText("");
+    }
+    else
+    {
+        QFile file(filename);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            THROW(FileAccessException, "Could not open file for reading: '" + filename + "'!");
+        }
+        ui->plain->setPlainText(file.readAll());
+
+        addRecentFile(filename);
+    }
+    modified_ = false;
+    updateWindowTitle();
+}
+
+void MainWindow::updateWindowTitle()
+{
+    setWindowTitle(qApp->applicationName() + " - " + file_ + (modified_ ? "*" : ""));
+}
+
+void MainWindow::addRecentFile(QString filename)
+{
+    QStringList files = Settings::stringList("recent_files");
+
+    files.prepend(filename.replace("\\", "/"));
+    files.removeDuplicates();
+    while(files.count()>15) files.pop_back();
+
+    Settings::setStringList("recent_files", files);
+
+    updateRecentFilesMenu();
+}
+
+void MainWindow::updateRecentFilesMenu()
+{
+    ui->menuRecentFiles->clear();
+
+    QStringList files = Settings::stringList("recent_files");
+    foreach(QString file, files)
+    {
+        ui->menuRecentFiles->addAction(file, this, SLOT(openRecentFile()));
+    }
 }
 
 QString MainWindow::markdown(QString in)
--- src/Underverse/mainwindow.h
+++ src/Underverse/mainwindow.h
@@ -15,13 +15,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     static QString markdown(QString in);
+    void loadFile(QString filename);
+    void updateWindowTitle();
+    void addRecentFile(QString filename);
+    void updateRecentFilesMenu();
 
 public slots:
-    void on_actionAbout_triggered(bool checked);
-    void updateHtml();
+    void on_actionAbout_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionClose_triggered();
+    void textChanged();
+    void openRecentFile();
 
 private:
     Ui::MainWindow *ui;
+    QString file_;
+    bool modified_;
 };
 
 #endif // MAINWINDOW_H
